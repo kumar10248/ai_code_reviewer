aaa9ba109f284578bd490b73b6604795a3ac54c1
kumar10248 <kumardevashish000@gmail.com>
2026-04-18 21:10:25 +0530

    feat(analytics): integrate Vercel Analytics into the layout and update dependencies

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,10 +1,13 @@
 // app/layout.tsx
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+        <Analytics />
+      </body>
     </html>
   )
 }
